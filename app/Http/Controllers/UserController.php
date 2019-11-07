d31c7dd5cc723ea224cfe0d6cccd8e6e4079b5a0
fazaimron27 <cincaumyself@gmail.com>
2019-11-07 19:06:34 +0700

    transformers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use App\Transformers\UserTransformer;
 
 class UserController extends Controller
 {
@@ -11,6 +12,9 @@ class UserController extends Controller
     {
         $users = $user->all();
 
-        return response()->json($users);
+        return fractal()
+            ->collection($users)
+            ->transformWith(new UserTransformer)
+            ->toArray();
     }
 }
